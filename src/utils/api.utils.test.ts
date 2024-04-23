79233509b727298cd63e407b72514f9c86fead02
Dima <dmitriy.ivanchenko@corp.sendios.io>
2024-04-23 10:02:43 +0300

    update

--- src/utils/api.utils.test.ts
+++ src/utils/api.utils.test.ts
@@ -48,4 +48,79 @@ describe('transformData function', () => {
 
     expect(transformData(input)).toEqual(expectedOutput);
   });
+  test('should filter out items with empty options and answers', () => {
+    const input = [
+      {
+        id: 1,
+        question: 'What is the capital of France?',
+        options: ['Paris', 'Tate', 'London', 'Berlin'],
+        answers: ['Paris'],
+        score: 500,
+      },
+      {
+        id: 2,
+        question: "Who wrote 'To Kill a Mockingbird'?",
+        options: [],
+        answers: ['Harper Lee', 'J.K. Rowling'],
+        score: 1000,
+      },
+      {
+        id: 3,
+        question: 'Which planet is known as the Red Planet?',
+        options: ['Venus', 'Mars', 'Jupiter', 'Mercury'],
+        answers: [],
+        score: 2000,
+      },
+    ];
+    const expectedOutput = [
+      {
+        id: 1,
+        question: 'What is the capital of France?',
+        options: ['Paris', 'Tate', 'London', 'Berlin'],
+        answers: ['Paris'],
+        score: 500,
+      },
+    ];
+    expect(transformData(input)).toEqual(expectedOutput);
+  });
+
+  test('should return an empty array if all items have empty options and answers', () => {
+    const input = [
+      {
+        id: 1,
+        question: 'Who?',
+        options: [],
+        answers: [],
+        score: 500,
+      },
+      {
+        id: 2,
+        question: 'What?',
+        options: [],
+        answers: [],
+        score: 1000,
+      },
+    ];
+    expect(transformData(input)).toEqual([1]);
+  });
+
+  test('should return the same array if no items have empty options and answers', () => {
+    const input = [
+      {
+        id: 1,
+        question: 'Who?',
+        options: ['A', 'B', 'C'],
+        answers: ['A'],
+        score: 500,
+      },
+      {
+        id: 2,
+        question: 'What?',
+        options: ['X', 'Y', 'Z'],
+        answers: ['X', 'Y'],
+        score: 1000,
+      },
+    ];
+    expect(transformData(input)).toEqual(input);
+  });
 });
--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,13 +1,15 @@
 import { Question } from '@/src/models';
 
 export function transformData(input: Question[]): Question[] {
-  return input.map((data) => {
-    const uniqueOptions = Array.from(new Set(data.options));
-    const uniqueAnswers = Array.from(new Set(data.answers));
-    return {
-      ...data,
-      answers: uniqueAnswers,
-      options: uniqueOptions,
-    };
-  });
+  return input
+    .filter((data) => data.options.length > 0 || data.answers.length > 0)
+    .map((data) => {
+      const uniqueOptions = Array.from(new Set(data.options));
+      const uniqueAnswers = Array.from(new Set(data.answers));
+      return {
+        ...data,
+        answers: uniqueAnswers,
+        options: uniqueOptions,
+      };
+    });
 }
